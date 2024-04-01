53d045f78bd4bdf8cbd9bc7b83812d71aee330b5
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-04-01 22:38:04 +0300

    Initial commit

---
+++ src/test/java/ru/vsu/cs/tp/recipesServerApplication/RecipesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.tp.recipesServerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
