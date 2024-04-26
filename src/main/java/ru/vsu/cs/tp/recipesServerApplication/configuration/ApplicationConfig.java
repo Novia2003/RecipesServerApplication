a71789d8f830d09b73e277b89873d6825be9e359
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-04-26 17:12:55 +0300

    recipes-23 Backend: регистрация/авторизация

--- src/main/java/ru/vsu/cs/tp/recipesServerApplication/configuration/ApplicationConfig.java
+++ src/main/java/ru/vsu/cs/tp/recipesServerApplication/configuration/ApplicationConfig.java
@@ -1,14 +1,12 @@
 package ru.vsu.cs.tp.recipesServerApplication.configuration;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
